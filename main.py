e13a877b7f5836209ba96fe9c31db40c1d2df62f
Yuichiro Yamashita <xydybaseball@gmail.com>
2020-09-07 15:38:50 +0900

    キー重複がある場合に事前に削除処理を実行してからINSERTする処理を実装しました。

--- main.py
+++ main.py
@@ -1,15 +1,71 @@
 import pandas
-from sqlalchemy import create_engine
+import mysql.connector
 import Constants
 
 CSV_FILE_PATH = "csv/data.csv"
 
+mysql_config = {
+    'user': Constants.USER,
+    'password': Constants.PASSWORD,
+    'host': Constants.HOST,
+    'port': Constants.PORT,
+    'database': Constants.DATABASE_NAME,
+    'charset': 'utf8'
+}
 
-def get_mysql_connection():
-    return create_engine('mysql://%s:%s@%s:%s/%s' % (
-        Constants.USER, Constants.PASSWORD, Constants.HOST, Constants.PORT, Constants.DATABASE_NAME))
+
+def main():
+    connection = mysql.connector.connect(user=Constants.USER, password=Constants.PASSWORD, host=Constants.HOST,
+                                         port=Constants.PORT,
+                                         database=Constants.DATABASE_NAME, charset="utf8")
+
+    try:
+
+        # extract exists gu_id.
+        cursor = connection.cursor(buffered=True)
+
+        try:
+
+            cursor.execute("SELECT %s FROM %s" % (Constants.TABLE_PRIMARY_KEY_NAME, Constants.TABLE_NAME))
+            existsKeys = [item[0] for item in cursor.fetchall()]
+
+            # read csv file.
+            csv_data = pandas.read_csv(CSV_FILE_PATH)
+
+            # delete exists rows.
+            updatableKeys = []
+            for index, row in csv_data.iterrows():
+                if row[Constants.TABLE_PRIMARY_KEY_NAME] in existsKeys:
+                    updatableKeys.append(row[Constants.TABLE_PRIMARY_KEY_NAME])
+            if len(updatableKeys) > 0:
+                deleteStatement = "DELETE FROM %s WHERE " % Constants.TABLE_NAME
+                for index in range(len(updatableKeys)):
+                    deleteStatement += (" OR " if (index > 0) else "") + "%s = %s" % (
+                        Constants.TABLE_PRIMARY_KEY_NAME, "%s")
+                cursor.execute(deleteStatement, updatableKeys)
+
+            # insert data.
+            for index, row in csv_data.iterrows():
+                cursor.execute(
+                    "INSERT INTO %s (%s) VALUES (%s)" % (
+                        Constants.TABLE_NAME, ",".join(row.keys().values),
+                        ",".join(["'" + item + "'" for item in row.values])))
+
+            # commit data
+            connection.commit()
+
+        finally:
+            # close connection
+            cursor.close()
+
+    finally:
+        # close connection
+        connection.close()
 
 
-with get_mysql_connection().begin() as connection:
-    df = pandas.read_csv(CSV_FILE_PATH)
-    df.to_sql(Constants.TABLE_NAME, connection, if_exists='append', index=None)
+if __name__ == "__main__":
+    main()
+
+# pandas#to_sql can't check any column data is already exists or not.
+# "if_exists='replace'" option's effect is that drop the table before inserting new values.
+# ref : https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_sql.html
